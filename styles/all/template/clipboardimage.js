e3b26f712159d795828fdd007bf254d93992af96
Jakub Senko <jakubsenko@gmail.com>
2018-12-28 10:40:17 +0100

    Automatically inline pasted image

--- styles/all/template/clipboardimage.js
+++ styles/all/template/clipboardimage.js
@@ -1,3 +1,4 @@
+// handle paste event
 window.addEventListener('paste', function(e) {
 	if (e.clipboardData === false || e.clipboardData.items == false) {
 		return;
@@ -12,4 +13,14 @@ window.addEventListener('paste', function(e) {
 		phpbb.plupload.uploader.addFile(items[i].getAsFile());
 	}
 	phpbb.plupload.uploader.start();
+});
+
+// place new image inline
+phpbb.plupload.uploader.bind('FileUploaded', function(up, file, response) {
+	try {
+		json = JSON.parse(response.response);
+		if (typeof json.title === 'undefined' && !json.error && file.status === plupload.DONE) {
+			attachInline(phpbb.plupload.getIndex(json.data[0].attach_id), json.data[0].real_filename);
+		}
+	} catch (e) {}
 });
