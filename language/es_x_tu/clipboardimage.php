ab5693e71a484f67d4a6690e252e9e07326c3a08
Jakub Senko <jakubsenko@gmail.com>
2019-04-12 14:03:02 +0200

    Add es and es_x_tu translations

---
+++ language/es_x_tu/clipboardimage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Clipboard Image. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2019 Jakub Senko
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'CLIPBOARDIMAGE_COPY'	=> 'Tu portapapeles contiene texto e imagen. ¿Qué artículo quieres pegar?',
+	'CLIPBOARDIMAGE_TEXT'	=> 'Texto',
+	'CLIPBOARDIMAGE_IMAGE'	=> 'Imagen',
+	'CLIPBOARDIMAGE_BOTH'	=> 'Ambos',
+));
